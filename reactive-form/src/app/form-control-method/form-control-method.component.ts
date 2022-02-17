871ce699fd59648cec5d9449a5ff7a6251909e50
fahadaleem <faleem396@gmail.com>
2022-02-17 12:11:02 +0500

    Form Control done.

--- reactive-form/src/app/form-control-method/form-control-method.component.ts
+++ reactive-form/src/app/form-control-method/form-control-method.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control-method',
@@ -9,13 +9,18 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class FormControlMethodComponent implements OnInit {
   constructor() {}
   // Method 1
-  username = new FormControl('');
-  email = new FormControl();
-  password = new FormControl('');
+  // username = new FormControl('');
+  // email = new FormControl();
+  // password = new FormControl('');
 
   ngOnInit(): void {}
+  myForm = new FormGroup({
+    username: new FormControl('Muhammad Fahad Aleem'),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   onSubmit(formData: FormGroup) {
-    console.log(formData);
+    console.log(formData.value);
   }
 }
